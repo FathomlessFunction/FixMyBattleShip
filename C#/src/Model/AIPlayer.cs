2b2b349eaa5ae184c705ea8c4e48464b93c74105
James H <jamesherrero@gmail.com>
2019-04-09 21:45:37 +1000

    fixed operator overloard to use referencesEquals in stances that it will = null

--- C#/src/Model/AIPlayer.cs
+++ C#/src/Model/AIPlayer.cs
@@ -67,7 +67,8 @@ namespace Battleships
         /// <returns>true if location 1 and location 2 are at the same spot</returns>
             public static bool operator ==(Location @this, Location other)
             {
-                return @this != null && other != null && @this.Row == other.Row && @this.Column == other.Column;
+                return !ReferenceEquals(@this, null) && !ReferenceEquals(other, null) && @this.Row == other.Row && @this.Column == other.Column;
+                //  return @this != null && other != null && @this.Row == other.Row && @this.Column == other.Column;
             }
 
             /// <summary>
@@ -78,7 +79,9 @@ namespace Battleships
         /// <returns>true if location 1 and location 2 are not at the same spot</returns>
             public static bool operator !=(Location @this, Location other)
             {
-                return @this == null || other == null || @this.Row != other.Row || @this.Column != other.Column;
+
+                return ReferenceEquals(@this, null) || ReferenceEquals(other, null) || @this.Row != other.Row || @this.Column != other.Column;
+                //  return @this == null || other == null || @this.Row != other.Row || @this.Column != other.Column;
             }
         }
 
