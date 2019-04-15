cd3439be981a8e0f41c99605faabd2f6d89f1f54
SARAH <MistaMishMash@users.noreply.github.com>
2019-04-15 22:56:13 +1000

    SV- fixed ships being placed 3 spaces lower than where clicked, see line 104 in DeploymentController.cs.

--- C#/src/DeploymentController.cs
+++ C#/src/DeploymentController.cs
@@ -101,7 +101,10 @@ static class DeploymentController
 		//Calculate the row/col clicked
 		int row = 0;
 		int col = 0;
-		row = Convert.ToInt32(Math.Floor((mouse.Y) / (UtilityFunctions.CELL_HEIGHT + UtilityFunctions.CELL_GAP)));
+        // Sarah: fixed ships being deployed 3 tiles beneath where the user clicks
+        // changed added '- UtilityFunctions.FIELD_TOP' to calculation of 'row', 
+        // to account for the displacement of the grid from the top of the screen
+        row = Convert.ToInt32(Math.Floor((mouse.Y - UtilityFunctions.FIELD_TOP) / (UtilityFunctions.CELL_HEIGHT + UtilityFunctions.CELL_GAP)));
 		col = Convert.ToInt32(Math.Floor((mouse.X - UtilityFunctions.FIELD_LEFT) / (UtilityFunctions.CELL_WIDTH + UtilityFunctions.CELL_GAP)));
 
 		if (row >= 0 & row < GameController.HumanPlayer.PlayerGrid.Height) {
